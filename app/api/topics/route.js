ad27079c7f345876d5b66489587ce784a6b93007
chetankronad06 <chetankronad06@gmail.com>
2025-03-15 21:42:36 +0530

    Initial commit

---
+++ app/api/topics/route.js
@@ -0,0 +1,38 @@
+import connectMongoDB from "@/libs/mongodb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const { title, description , author} = await request.json(); 
+  await connectMongoDB();
+  console.log(title, description)
+  await Topic.create({ title, description , author});
+  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topic.find();
+  return NextResponse.json({ topics });
+}
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Topic.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
+}
+// export async function PUT(request, { params }) {
+//   const { id } = params;
+//   const { newTitle: title, newDescription: description } = await request.json();
+//   await connectMongoDB();
+//   await Topic.findByIdAndUpdate(id, { title, description });
+//   return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+// }
+
+// export async function PATCH(request) {
+//   const data = await request.json();
+//   console.log(data);
+//   const res = await Topic.findByIdAndUpdate(data._id,data);
+//   return NextResponse.json({ message: res }, { status: 200 });
+// }
---
+++ components/RemoveButton.js
@@ -0,0 +1,10 @@
+"use client";
+import { HiOutlineTrash } from "react-icons/hi";
+
+export default function RemoveBtn({ id }) {
+  return (
+    <button  className="text-red-400">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
+}
